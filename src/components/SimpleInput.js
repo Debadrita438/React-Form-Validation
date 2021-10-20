ccd7c2f4c5578832c3251f5ff8ffc24eeeaa9861
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-20 17:13:01 +0530

    Applying entire form validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SimpleInput = () => {
     const [input, setInput] = useState('');
     const [nameTouched, setNameTouched] = useState(false);
 
+    // Either this, but not needed here because of extra component re-evaluation
+    // const [formIsValid, setFormIsValid] = useState(false);
+
     const isValid = input.trim() !== '';
     const nameInputIsInvalid = !isValid && nameTouched;
+
+    // Or this
+    let formIsValid = false;
+
+    if(isValid) {
+        formIsValid = true;
+    }
+
+    // Either this, but not needed here because of extra component re-evaluation
+    // useEffect(() => {
+    //     if(isValid) {
+    //         setFormIsValid(true);
+    //     } else {
+    //         setFormIsValid(false);
+    //     }
+    // }, [isValid])
 
     const inputChangeHandler = event => {
         setInput(event.target.value);
@@ -44,7 +63,7 @@ const SimpleInput = () => {
                 {nameInputIsInvalid && <p className='error-text'>Name must not be valid</p>}
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button disabled={!formIsValid}>Submit</button>
             </div>
       </form>
     );
